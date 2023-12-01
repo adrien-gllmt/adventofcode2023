598cc194e90cb5dce7ee9ffcdd263713a97952f0
Adrien GUILLEMOT <a.guillemot@opticiens-atol.com>
2023-12-01 16:59:03 +0100

    feat(day1): started part 2

--- day_1/main.rb
+++ day_1/main.rb
@@ -16,3 +16,29 @@ end
 
 puts "Le résultat de la partie 1 est #{sum_total('day_1/input.txt')}"
 # END OF PART 1
+
+# BEGINNING OF PART 2
+def find_last_word(line, last_word)
+  %w[one two three four five six seven eight nine].each do |n|
+    last_word = { 'index' => line =~ /#{n}/, 'word' => n } if line =~ /#{n}/ && (line =~ /#{n}/) > last_word['index']
+  end
+end
+
+def find_last_num(line, last_num)
+  10.times do |n|
+    last_num = { 'index' => line =~ /#{n}/, 'num' => n } if line =~ /#{n}/ && (line =~ /#{n}/) > last_num['index']
+  end
+end
+
+def sum_total_with_letters(file)
+  total = 0
+  File.foreach(file) do |line|
+    last_word = { 'index' => 0, 'word' => '' }
+    last_num = { 'index' => 0, 'num' => 0 }
+    find_last_word(line, last_word)
+    find_last_num(line, last_num)
+  end
+  total
+end
+
+sum_total_with_letters('day_1/input.txt')
